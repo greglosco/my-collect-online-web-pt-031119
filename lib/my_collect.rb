1fc01010710c7db2b28bbb5d95b06cd89eb7998d
Gregory Losco <gtlosco@gmail.com>
2019-03-22 01:49:15 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,3 +1,12 @@
 def my_collect(collection)
+  i = 0 
+  modified_collection = []
+  while i < collection.length
+  collection <<
+  modified_collection
+  yield(collection[])
+  i += 1 
+end
+modified_collection
 end
 
